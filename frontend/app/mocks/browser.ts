5e2e71bbcb9aad42433a3e809125cb75519ff99b
kokai <ryo.kokai@givery.co.jp>
2025-07-11 15:54:29 +0900

    中間ファイルを掃除

--- frontend/app/mocks/browser.ts
+++ frontend/app/mocks/browser.ts
@@ -1,6 +1,6 @@
 // NOTE: msw環境でモックAPIが動作するようなコードを残しておく。基本的に使わない方針。
 import { setupWorker } from 'msw/browser';
-// NOTE: ↓このimport文の書き方で./handlers/index.tsを自動で読み込んでhandlersを見つけてくれる。
-import { handlers } from './handlers';
+// NOTE: 認証関連のモックハンドラーを直接インポート
+import { authHandlers } from './handlers/authHandlers';
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...authHandlers);
--- frontend/app/mocks/server.ts
+++ frontend/app/mocks/server.ts
@@ -1,6 +1,6 @@
 // NOTE: msw環境でモックAPIが動作するようなコードを残しておく。基本的に使わない方針。
 // app/mocks/server.ts
 import { setupServer } from 'msw/node';
-import { handlers } from './handlers';
+import { authHandlers } from './handlers/authHandlers';
 
-export const server = setupServer(...handlers);
+export const server = setupServer(...authHandlers);
